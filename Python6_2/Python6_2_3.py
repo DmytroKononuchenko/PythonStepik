93489fba28cfcecc4f9c841b4aed314c7ddd23dc
Дмитро <konuk008@ukr.net>
2021-01-12 21:14:25 +0100

    6_2

---
+++ Python6_2/Python6_2_3.py
@@ -0,0 +1,12 @@
+from math import *
+a = float(input())
+b = float(input())
+
+q = (a + b) / 2
+w = sqrt(a * b)
+e = (2 * a * b) / (a + b)
+r = sqrt(((a ** 2) + (b ** 2)) / 2)
+print(q)
+print(w)
+print(e)
+print(r)
